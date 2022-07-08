0ea93b81f8df9d5f85ce00bf27c3b44ebc0476c6
Guillaume D <g.deloince+dev@gmail.com>
2022-07-08 14:31:47 +0200

    Unknown routing

--- src/Menu/GDPRMenuListener.php
+++ src/Menu/GDPRMenuListener.php
@@ -15,7 +15,7 @@ final class GDPRMenuListener
         $gdprMenu = $menu->addChild('gdpr');
         $gdprMenu
             ->addChild('sylius.ui.admin.synolia_gdpr.advanced_actions.title', [
-                'route' => 'sylius_gdpr_advanced_actions',
+                'route' => 'advanced_actions',
             ]);
     }
 }
