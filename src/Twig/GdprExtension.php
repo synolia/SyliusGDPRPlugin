b90e93bc9e51606f953fdfb7053afa2ebfa0209b
Olivier ALLAIN <olivier.allain@synolia.com>
2023-04-07 19:24:50 +0200

    fix sylius plus RBAC check

---
+++ src/Twig/GdprExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Synolia\SyliusGDPRPlugin\Twig;
+
+use Twig\Environment;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class GdprExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('sylius_plus_rbac_gdpr_has_permission', [$this, 'hasPermission'], ['needs_environment' => true]),
+        ];
+    }
+
+    public function hasPermission(Environment $env, string $permission): bool
+    {
+        $function = $env->getFunction('sylius_plus_rbac_has_permission');
+
+        if ($function !== null && is_callable($function->getCallable())) {
+            return $function->getCallable()($permission);
+        }
+
+        return true;
+    }
+}
